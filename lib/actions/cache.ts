a495119a85594aa9c9d5a30cddd624e3bffd3518
William MacBook <wwilliamw24@gmail.com>
2025-03-07 11:50:56 -0800

    caching available for donors

--- lib/actions/cache.ts
+++ lib/actions/cache.ts
@@ -1,6 +1,8 @@
 'use server';
 
 import redis from "../redis";
+import { prisma } from '@/lib/prisma';
+import { Donor } from "@prisma/client";
 
 export async function fetchWithCache(url: string) {
   const cacheKey = `cache_key:${url}`;
@@ -20,5 +22,23 @@ export async function fetchWithCache(url: string) {
   }
   catch (e: any) {
     throw new Error(e.message);
+  }
+}
+
+export async function cacheDonors(): Promise<{success: boolean, data: Donor[] | null}>{
+  const cacheKey = "donors";
+  const cachedData = await redis.get(cacheKey);
+
+  if (cachedData) {
+    return { success: true, data: JSON.parse(cachedData)};
+  }
+
+  try {
+    const donors = await prisma.donor.findMany();
+    await redis.set(cacheKey, JSON.stringify(donors), "EX", 3600);
+    return { success: true, data: donors };
+  } catch (error) {
+    console.error("[getDonor] Failed to fetch donors:", error);
+    return { success: false, data: null };
   }
 }
