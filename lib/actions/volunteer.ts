d74ea3601c2c6f9a09a371d7d4f6d1ceeca387a3
Daniel Teo <dteo318@gmail.com>
2025-03-09 23:42:32 -0700

    Created volunteer sign up

--- lib/actions/volunteer.ts
+++ lib/actions/volunteer.ts
@@ -1,11 +1,33 @@
 'use server';
 import { prisma } from '@/lib/prisma';
 import { validateTimeSlot } from '@/lib/utils/dates';
-import type { SignupDetails, VolunteerSignupWithShelter } from '@/types/shelter';
+import type {
+  SignupDetails,
+  VolunteerSignupWithShelter,
+} from '@/types/shelter';
 import type {
   VolunteerSignupCreationAttributes,
   ActionResult,
 } from '@/types/models';
+import { Prisma, Volunteer } from '@prisma/client';
+
+export async function addVolunteer(
+  volunteer: Prisma.VolunteerCreateInput,
+): Promise<ActionResult<Volunteer>> {
+  try {
+    console.log('Adding new volunteer...');
+    const newVolunteer = await prisma.volunteer.create({
+      data: volunteer,
+    });
+    console.log('Inserted volunteer:', newVolunteer);
+    return { success: true, data: newVolunteer };
+  } catch (error) {
+    console.error('Error inserting volunteer:', error);
+    return {
+      success: false,
+    };
+  }
+}
 
 /**
  * Action that creates a new volunteer signup
@@ -59,8 +81,10 @@ export async function signupVolunteer({
   }
 }
 
-export async function getVolunteerDashboard(): Promise<ActionResult<VolunteerSignupWithShelter[]>>{
-  try{
+export async function getVolunteerDashboard(): Promise<
+  ActionResult<VolunteerSignupWithShelter[]>
+> {
+  try {
     const volunteerSignups = await prisma.volunteerSignup.findMany({
       include: {
         shelter: {
@@ -72,8 +96,8 @@ export async function getVolunteerDashboard(): Promise<ActionResult<VolunteerSig
       },
     });
 
-    return { success: true, data: volunteerSignups }    
-  } catch{
+    return { success: true, data: volunteerSignups };
+  } catch {
     throw new Error('[VolunteerSignup] Database operation failed');
   }
 }
